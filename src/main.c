d275d4ac50d0a6cc6b5b96ef288c01d4e9db17ac
Irjan Vuckovic Olsen <irjan.olsen@gmail.com>
2024-08-01 21:38:14 +0200

    Changed to bare metal RISC-V

--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
 extern long long add(long long a, long long b);
 
-int main() {
-    return add(5, 6);
+unsigned char * uart = (unsigned char *)0x10000000; 
+void putchar(char c) {
+	*((unsigned char *)0x10000000) = c;
+	return;
+}
+ 
+void print(const char * str) {
+	while(*str != '\0') {
+		putchar(*str);
+		str++;
+	}
+	return;
+}
+
+int kernel_main() {
+    long long i = add(4, 5);
+    char n[] = {'0' + i, '\0'};
+    print(n);
+    print("Hello, rv64!\n");
+    while(1);
 }
